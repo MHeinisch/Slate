6b167a778a727dfa50c5903563c18e0d014d042e
Jared Merten <jarmerten@gmail.com>
2016-05-25 15:40:39 -0500

    Added Schedule Database

--- Slate/Models/BusinessInfo.cs
+++ Slate/Models/BusinessInfo.cs
@@ -7,5 +7,41 @@ namespace Slate.Models
 {
     public class BusinessInfo
     {
+        [Key]
+        public int id { get; set; }
+
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Company Name")]
+        public string CompanyName { get; set; }
+
+        [Required]
+        [Display(Name = "Phone Number")]
+        public int PhoneNumber { get; set; }
+
+        [Required]
+        [Display(Name = "Address")]
+        public string Address { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        public string UserId { get; set; }
     }
 }
