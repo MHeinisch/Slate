8b55873659dd3f5903c047de1a86d3ddcdbfcf5b
Michael Heinisch <venomx411@gmail.com>
2016-05-25 14:48:41 -0500

    Project creation, template changed to Slate stuff

---
+++ Slate/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Slate.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "A start-up based in Milwaukee, WI creating revolutionary software for small and local restaurants and bars,";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact JIM B, the creators of Slate.";
+
+            return View();
+        }
+
+    }
+}
