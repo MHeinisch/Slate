579685b60793a6fb2efd3aa815ec2bd268160a86
Jared Merten <jarmerten@gmail.com>
2016-05-26 16:16:10 -0500

    All database models added but empty

---
+++ Slate_Reborn/Models/Roles.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Slate_Reborn.Models
+{
+    public class Roles
+    {
+        [Key]
+
+        public int ShiftId { get; set; }
+
+    }
+}
---
+++ Slate_Reborn/Models/Shift.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Slate_Reborn.Models
+{
+    public class Shift
+    {
+        [Key]
+        [ForeignKey("BusinessInfo")]
+        public int BusinessInfoId { get; set; }
+        public virtual BusinessInfo BusinessInfo { get; set; }
+    }
+}
