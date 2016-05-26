4409309931408a1e18a8883275fe976a4682ccdf
Jared Merten <jarmerten@gmail.com>
2016-05-26 16:35:13 -0500

    Shift model built

--- Slate_Reborn/Models/Shift.cs
+++ Slate_Reborn/Models/Shift.cs
@@ -10,8 +10,19 @@ namespace Slate_Reborn.Models
     public class Shift
     {
         [Key]
-        [ForeignKey("BusinessInfo")]
-        public int BusinessInfoId { get; set; }
-        public virtual BusinessInfo BusinessInfo { get; set; }
+        
+        public int ShiftId { get; set; }
+
+        [ForeignKey("Roles")]
+        public int RoleId { get; set; }
+        public virtual Roles Roles { get; set; }
+        public int Start { get; set; }
+        public int End { get; set; }
+
+        [ForeignKey("Day")]
+        public int DayId { get; set; }
+        public virtual Day Day { get; set; }
+
+
     }
 }
